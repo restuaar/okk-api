deae271282933487082a857073cba7f926eb3461
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2024-07-08 18:45:09 +0700

    fix(winston): development env salah transport

--- src/config/winston.config.ts
+++ src/config/winston.config.ts
@@ -7,17 +7,17 @@ ConfigModule.forRoot();
 
 const transports: Array<any> = [];
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
   transports.push(
-    new winston.transports.Console({
+    new winston.transports.DailyRotateFile({
+      filename: 'logs/application-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
       format: winston.format.combine(
         winston.format.timestamp(),
-        winston.format.colorize(),
-        winston.format.printf(
-          ({ timestamp, level, message, context, trace }) => {
-            return `${timestamp} [${context}] ${level}: ${message}${trace ? `\n${trace}` : ''}`;
-          },
-        ),
+        winston.format.json(),
       ),
     }),
   );
@@ -37,15 +37,13 @@ if (
 }
 
 transports.push(
-  new winston.transports.DailyRotateFile({
-    filename: 'logs/application-%DATE%.log',
-    datePattern: 'YYYY-MM-DD',
-    zippedArchive: true,
-    maxSize: '20m',
-    maxFiles: '14d',
+  new winston.transports.Console({
     format: winston.format.combine(
       winston.format.timestamp(),
-      winston.format.json(),
+      winston.format.colorize(),
+      winston.format.printf(({ timestamp, level, message, context, trace }) => {
+        return `${timestamp} [${context}] ${level}: ${message}${trace ? `\n${trace}` : ''}`;
+      }),
     ),
   }),
 );
